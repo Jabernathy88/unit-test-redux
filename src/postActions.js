2e3ecb206bff69d70adf776935bf920c1f0d308e
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-14 12:53:15 -0400

    Setup basic redux.

---
+++ src/postActions.js
@@ -0,0 +1,72 @@
+// import axios from 'axios';
+
+function getPosts() {
+  return fetch("/posts")
+    .then(handleErrors)
+    .then(res => res.json());
+}
+
+function fakeGetPosts() {
+  return new Promise(resolve => {
+
+    setTimeout(
+      () => 
+        resolve({
+          posts: [
+            {
+              id: 0,
+              name: "Apple"
+            },
+            {
+              id: 1,
+              name: "Bananas"
+            },
+            {
+              id: 2,
+              name: "Strawberries"
+            }
+          ]
+        }),
+      1000
+    );
+  });
+}
+
+export function fetchPosts() {
+  return dispatch => {
+    dispatch(fetchPostsBegin());
+    return fakeGetPosts()
+    .then(json => {
+      dispatch(fetchPostsSuccess(json.posts));
+      return json.posts;
+    })
+    .catch(error => 
+      dispatch(fetchPostsFailure(error))
+    );
+  };
+}
+
+// handle HTTP errors // TODO: REMOVE
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+export const FETCH_POSTS_BEGIN = "FETCH_posts_BEGIN";
+export const FETCH_POSTS_SUCCESS = "FETCH_posts_SUCCESS";
+export const FETCH_POSTS_FAILURE = "FETCH_posts_FAILRE";
+
+export const fetchPostsBegin = () => ({
+  type: FETCH_POSTS_BEGIN
+})
+
+export const fetchPostsSuccess = posts => ({
+  type: FETCH_POSTS_SUCCESS,
+  payload: { posts }
+})
+
+export const fetchPostsFailure = error => ({
+  type: FETCH_POSTS_FAILURE
+})
